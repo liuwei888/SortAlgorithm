1c9e78280d45f055b7fb72db8eda093ecc5f58a8
liu wei <liuwei888@mails.ccnu.edu.cn>
2015-08-28 00:49:01 +0800

    insert.cpp

---
+++ insert.cpp
@@ -0,0 +1,34 @@
+/*************************************************************************
+    > File Name: insert.cpp
+    > Author: liuwei
+    > Mail: 271551048@qq.com
+    > Created Time: 2015年08月28日 星期五 00时15分30秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+void InsertSort(int array[], int length)
+{
+        if(NULL == array || length < 0)
+                return ;
+
+        for(int i = 1; i < length; i++)
+        {
+                int temp = array[i]; //将array[i]保存起来
+                int j = i;
+                while(j > 0 && temp < array[j-1])
+                {
+                        array[j] = array[j-1];
+                        j--;
+                }
+                array[j] = temp;
+        }
+}
+
+int main()
+{
+        int a[5] = {13, 18, 5, 2, 10};
+        InsertSort(a, 5);
+        for(int i = 0; i < 5; i++)
+                cout << a[i] << " ";
+}
