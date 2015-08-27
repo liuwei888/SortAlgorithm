e7c230f1d7ad8a5d1b9a1da1f37d0753d5186988
root <root@ccnu.(none)>
2015-08-27 23:53:57 +0800

    1st edition

---
+++ bubble.cpp
@@ -0,0 +1,30 @@
+/*************************************************************************
+    > File Name: bubble.cpp
+    > Author: liuwei
+    > Mail: 271551048@qq.com 
+    > Created Time: 2015年08月27日 星期四 23时40分43秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+void BubbleSort(int array[], int length)
+{
+	for(int i = 0; i < length; i++)
+		for(int j = length - 1; j > i; j--)
+		{
+			if(array[j] < array[j-1])
+			{
+				int temp = array[j];
+				array[j] = array[j-1];
+				array[j-1] = temp;
+			}
+		}
+}
+
+int main()
+{
+	int a[5] = {12, 23, 89, 6, 9};
+	BubbleSort(a, 5);
+	for(int i = 0; i < 5; i++)
+		cout << a[i] << " ";
+}
